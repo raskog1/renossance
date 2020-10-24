974238af124a67d833b234bb114f8d4e21dc231a
Ryan Skog <ryan.skog@outlook.com>
2020-10-24 15:23:54 -0700

    Added trackers to rooms, and wrote a route to toggle them complete/incomplete

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -25,6 +25,45 @@ router.post("/", auth, async (req, res) => {
     }
 });
 
+// @route   PUT api/rooms/trackers
+// @desc    Add a tracker to a room
+// @access  Private
+router.put("/trackers/:id", auth, async (req, res) => {
+    const tracker = { name: req.body.name, updatedBy: req.user.id };
+    try {
+        const room = await Room.findById(req.params.id);
+        room.trackers.push(tracker);
+        await room.save();
+        res.json(room);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+
+})
+
+// @route   PUT api/rooms/trackers/toggle/:id
+// @desc    Toggle tracker complete/incomplete
+// @access  Private
+router.put("/trackers/toggle/:id", auth, async (req, res) => {
+
+    try {
+        const room = await Room.findById(req.params.id);
+
+        for (let i = 0; i < room.trackers.length; i++) {
+            if (room.trackers[i].name === req.body.name) {
+                room.trackers[i].completed = !room.trackers[i].completed;
+            };
+        }
+        await room.save();
+        res.json(room);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+})
+
 // @route   GET api/rooms
 // @desc    Get all rooms from current logged in location
 // @access  Private
